beb932eb7499369596356309cf8c32307e88bf09
Gribbariga <mormir09@gmail.com>
2024-08-28 14:19:33 +0900

    add tarifs

--- src/Features/UploadWindow/UI/UploadWindow.tsx
+++ src/Features/UploadWindow/UI/UploadWindow.tsx
@@ -2,40 +2,54 @@ import { createPortal } from "react-dom";
 import { UploadWindowStyle } from "./UploadWindowStyle.ts";
 import { Heading, Text } from "@radix-ui/themes";
 import { UploadIcon } from "@radix-ui/react-icons";
-import { ChangeEvent, DragEvent, useEffect } from "react";
+import { ChangeEvent, DragEvent, useEffect, useState } from "react";
 import { createFolder } from "shared/API/storage/folder/api.ts";
 import { uploadFile } from "shared/API/storage/files/api.ts";
 import { useNavigate } from "react-router-dom";
 import { setCookie } from "shared/lib/helper/setCookie/setCookie.ts";
 import { getCookie } from "shared/lib/helper/getCookie/getCookie.ts";
+import { subscriptionSlice } from "src/entities/subscription/modal/subcriptionSlice.ts";
+import { ITariffs } from "shared/API/subscription/modal.ts";
 
 export const UploadWindow = () => {
   const navigation = useNavigate();
+  const { tariffs, subscription_id } = subscriptionSlice((state) => state);
+
+  const [tariffsState, setTarifsState] = useState<ITariffs | null>(null);
+
+  useEffect(() => {
+    if (tariffs) {
+      setTarifsState(tariffs[subscription_id]);
+    }
+  }, [tariffs, subscription_id]);
 
   const handleUploadFile = (event: DragEvent<HTMLDivElement>) => {
-    const files = event.dataTransfer.files;
-    createFolder({
-      lifetime: import.meta.env.VITE_GUEST_MAX_DAY_STORAGE,
-      download_password: false,
-    }).then(({ data }) => {
-      setCookie("folderId", data.folder_id);
-      console.log("?");
+    if (tariffsState) {
+      const files = event.dataTransfer.files;
 
-      console.log(files);
-      for (let i = 0; i < files.length; i++) {
-        console.log("start");
-        const fileSizeInMB = files[i].size / (1024 * 1024);
-        console.log(files[i]);
-        if (
-          i < import.meta.env.VITE_GUEST_MAX_COUNT_FILE &&
-          fileSizeInMB <= import.meta.env.VITE_GUEST_MAX_SIZE_FILE
-        ) {
-          uploadFile({ file: files[i], folder_id: data.folder_id });
+      createFolder({
+        lifetime: tariffsState.max_lifetime,
+        download_password: false,
+      }).then(({ data }) => {
+        setCookie("folderId", data.folder_id);
+        console.log("?");
+
+        console.log(files);
+        for (let i = 0; i < files.length; i++) {
+          console.log("start");
+          const fileSizeInMB = files[i].size;
+          console.log(files[i]);
+          if (
+            i < tariffsState.max_file_in_folder &&
+            fileSizeInMB <= +tariffsState.max_file_size
+          ) {
+            uploadFile({ file: files[i], folder_id: data.folder_id });
+          }
         }
-      }
-      navigation("/");
-    });
-    console.log(event.dataTransfer.files[0]);
+        navigation("/");
+      });
+      console.log(event.dataTransfer.files[0]);
+    }
   };
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
