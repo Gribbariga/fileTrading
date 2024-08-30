f8d29b497c3f7bcfe93a6f7a75f6d5802759c0e9
Gribbariga <mormir09@gmail.com>
2024-08-31 07:26:25 +0900

    add isGuest in sliceStorage

--- src/page/StorageViewPage/UI/StorageViewPage.tsx
+++ src/page/StorageViewPage/UI/StorageViewPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Container } from "shared/lib/Container/Container";
+import { getCookie } from "shared/lib/helper/getCookie/getCookie";
 import { storageSlice } from "src/entities/storage/modal/storageSlice";
 import styled from "styled-components";
 import { Layout } from "widgets/layout/publicApi";
@@ -10,12 +11,14 @@ import { StorageFileList } from "widgets/StorageFileList/UI/StorageFileList";
 import { StorageHeader } from "widgets/StorageHeader/UI/StorageHeader";
 
 const StorageViewPage = () => {
-  const { isLoading, getStorage } = storageSlice((state) => state);
+  const { isLoading, setIsGuest, getStorage } = storageSlice((state) => state);
   const { storageLink } = useParams();
 
   useEffect(() => {
     console.log(storageLink);
     if (storageLink) {
+      const id = getCookie("folderId");
+      setIsGuest(id === storageLink);
       getStorage(storageLink, "0");
     }
   }, [storageLink]);
