2ea342f37ab03ee309b121ea0df0b8d907359f2d
Gribbariga <mormir09@gmail.com>
2024-08-31 12:05:43 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
-import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "./",
+  base: "/",
   resolve: {
     alias: {
-      src: path.resolve(__dirname, "src"),
-      shared: path.resolve(__dirname, "src/shared"),
-      features: path.resolve(__dirname, "src/features"),
-      page: path.resolve(__dirname, "src/page"),
-      widgets: path.resolve(__dirname, "src/widgets"),
-      public: path.resolve(__dirname, "public"),
+      src: "src",
+      shared: "src/shared/*",
+      features: "src/features/*",
+      page: "src/page/*",
+      widgets: "src/widgets/*",
     },
   },
 
