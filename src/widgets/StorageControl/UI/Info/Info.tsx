4f2195ff456d15c510b7278c502753520f3f0ec7
Gribbariga <mormir09@gmail.com>
2024-08-31 13:22:49 +0900

    fix

--- src/widgets/StorageControl/UI/Info/Info.tsx
+++ src/widgets/StorageControl/UI/Info/Info.tsx
@@ -6,74 +6,84 @@ import { StorageDescription } from "src/features/StorageDescription/UI/StorageDe
 
 export const Info = () => {
   const { storage } = storageSlice((state) => state);
+  let result: {
+    name: string;
+    value: string | number;
+  }[] = [];
 
-  const result = [
-    {
-      name: "Создано",
-      value: formattedData(storage?.created_at || ""),
-    },
-    {
-      name: "Изменено",
-      value: formattedData(storage?.updated_at || ""),
-    },
-    {
-      name: "Автор",
-      value: "user2390857",
-    },
-    {
-      name: "Размер",
-      value: ((storage?.folder_size || 0) / 1024 / 1024 / 1024).toFixed(2),
-    },
-    {
-      name: "Скачано раз",
-      value: storage?.download_count,
-    },
-    {
-      name: "Просмотры",
-      value: "363",
-    },
-    {
-      name: "Заполнено",
-      value: "56%",
-    },
-  ];
+  if (storage) {
+    result = [
+      {
+        name: "Создано",
+        value: formattedData(storage?.created_at || ""),
+      },
+      {
+        name: "Изменено",
+        value: formattedData(storage?.updated_at || ""),
+      },
+      {
+        name: "Автор",
+        value: "user2390857",
+      },
+      {
+        name: "Размер",
+        value: ((storage?.folder_size || 0) / 1024 / 1024 / 1024).toFixed(2),
+      },
+      {
+        name: "Скачано раз",
+        value: storage?.download_count,
+      },
+      {
+        name: "Просмотры",
+        value: "363",
+      },
+      {
+        name: "Заполнено",
+        value: "56%",
+      },
+    ];
+  }
 
   return (
     <>
-      <InfoWrapperSC>
-        {result.map((item) => {
-          return (
-            <InfoItemSC key={item.name}>
-              <Text
-                size={"3"}
-                weight={"medium"}
-                align={"left"}
-                highContrast={false}
-              >
-                {item.name}
-              </Text>
-              <Text
-                size={"3"}
-                weight={"medium"}
-                align={"right"}
-                highContrast={true}
-              >
-                {item.value}
-              </Text>
-            </InfoItemSC>
-          );
-        })}
-      </InfoWrapperSC>
-      <Text
-        size={"3"}
-        weight={"medium"}
-        align={"left"}
-        highContrast={true}
-        mb={"-8"}
-      >
-        Описание хранилища
-      </Text>
-      <StorageDescription />
+      {storage && (
+        <>
+          <InfoWrapperSC>
+            {result.map((item) => {
+              return (
+                <InfoItemSC key={item.name}>
+                  <Text
+                    size={"3"}
+                    weight={"medium"}
+                    align={"left"}
+                    highContrast={false}
+                  >
+                    {item.name}
+                  </Text>
+                  <Text
+                    size={"3"}
+                    weight={"medium"}
+                    align={"right"}
+                    highContrast={true}
+                  >
+                    {item.value}
+                  </Text>
+                </InfoItemSC>
+              );
+            })}
+          </InfoWrapperSC>
+          <Text
+            size={"3"}
+            weight={"medium"}
+            align={"left"}
+            highContrast={true}
+            mb={"-8"}
+          >
+            Описание хранилища
+          </Text>
+          <StorageDescription />
+        </>
+      )}
     </>
   );
 };
