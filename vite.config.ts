77c83a6c3d4dae893a7bf142dacf976105dcab34
Gribbariga <mormir09@gmail.com>
2024-08-31 11:26:30 +0900

    fix builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   base: "./",
   resolve: {
     alias: {
-      src: "/src",
-      shared: "/src/shared",
-      features: "/src/features",
-      page: "/src/page",
-      widgets: "/src/widgets",
-      public: "/public",
+      src: path.resolve(__dirname, "src"),
+      shared: path.resolve(__dirname, "src/shared"),
+      features: path.resolve(__dirname, "src/features"),
+      page: path.resolve(__dirname, "src/page"),
+      widgets: path.resolve(__dirname, "src/widgets"),
+      public: path.resolve(__dirname, "public"),
     },
   },
 
@@ -23,12 +24,12 @@ export default defineConfig({
     },
     host: true,
     port: 4517,
-    proxy: {
-      "/api": {
-        target: "http://5.35.98.32/",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "http://5.35.98.32/",
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, ""),
+    //   },
+    // },
   },
 });
