cb05806bde1043b6f3e42de6c568cf846aa8a62c
Gribbariga <mormir09@gmail.com>
2024-09-04 01:41:52 +0900

    add 2fa

---
+++ src/features/TwoFA/UI/TwoFA.tsx
@@ -0,0 +1,113 @@
+import { ButtonUI } from "src/shared/ButtonUI/ButtonUI.tsx";
+import { TwoFAStyle } from "./TwoFAStyle.ts";
+import { Callout, Link, Text } from "@radix-ui/themes";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
+import { userSlice } from "src/entities/user/model/userSlice.ts";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+interface IData {
+  key: string;
+}
+
+export const TwoFA = () => {
+  const {
+    register: registerInput,
+    setError,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      key: "",
+    },
+  });
+  const { setToken } = userSlice((state) => state);
+
+  const [, setIsLoading] = useState(false);
+  const navigation = useNavigate();
+
+  const handleRegister = (data: IData) => {
+    setIsLoading(true);
+    // login(data)
+    //   .then(({ data }) => {
+    //     setIsLoading(false);
+    //     setToken(data.token);
+    //     navigation("/");
+    //   })
+    //   .catch((error: Error | AxiosError) => {
+    //     setIsLoading(false);
+    //     if (isAxiosError(error)) {
+    //       const errorMessage = backendCodeError[error.status || 500];
+    //       if (typeof errorMessage !== "string") {
+    //         setError("root", { message: errorMessage[error.message] });
+    //       } else {
+    //         setError("root", { message: errorMessage });
+    //       }
+    //     }
+    //   });
+  };
+
+  const error = errors.root?.message || errors.key?.message;
+
+  return (
+    <>
+      <FormSC onSubmit={handleSubmit(handleRegister)}>
+        <InputsWrapperSC>
+          <InputWrapperSC>
+            <Text
+              align={"left"}
+              highContrast={true}
+              size={"3"}
+              weight={"medium"}
+            >
+              Придумайте логин
+            </Text>
+            <TextFieldSC
+              size={"3"}
+              variant="surface"
+              {...registerInput("key", {
+                required: "Заполните все обязательные поля",
+              })}
+              placeholder="Защитный код"
+            />
+          </InputWrapperSC>
+        </InputsWrapperSC>
+
+        {error && (
+          <>
+            <CalloutSC color="red" size={"1"} mb={"5"} variant="soft">
+              <Callout.Icon>
+                <InfoCircledIcon />
+              </Callout.Icon>
+              <Callout.Text>{error}</Callout.Text>
+            </CalloutSC>
+          </>
+        )}
+
+        <ButtonsGroupSC>
+          <ButtonUI size={"2"} variant="solid" highContrast={false}>
+            Назад
+          </ButtonUI>
+          <ButtonUI
+            size={"2"}
+            variant="soft"
+            highContrast={false}
+            type="submit"
+          >
+            Продолжить
+          </ButtonUI>
+        </ButtonsGroupSC>
+      </FormSC>
+    </>
+  );
+};
+
+const {
+  FormSC,
+  CalloutSC,
+  TextFieldSC,
+  InputWrapperSC,
+  ButtonsGroupSC,
+  InputsWrapperSC,
+} = TwoFAStyle();
---
+++ src/features/TwoFA/UI/TwoFAStyle.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+import { Callout, TextField } from "@radix-ui/themes";
+
+const InputWrapperSC = styled("div")``;
+
+const ButtonsGroupSC = styled("div")`
+  width: 100%;
+  display: flex;
+  justify-content: end;
+  gap: var(--space-3);
+`;
+
+const InputsWrapperSC = styled("div")`
+  margin-bottom: var(--space-5);
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-4);
+`;
+
+const TextFieldSC = styled(TextField.Root)`
+  width: 100%;
+`;
+
+const CalloutSC = styled(Callout.Root)`
+  width: 100%;
+`;
+
+const FormSC = styled("form")`
+  width: 100%;
+`;
+
+const PasswordTitleWrapperSC = styled("div")`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: var(--space-1);
+`;
+
+export const TwoFAStyle = () => ({
+  FormSC,
+  CalloutSC,
+  TextFieldSC,
+  InputWrapperSC,
+  ButtonsGroupSC,
+  InputsWrapperSC,
+  PasswordTitleWrapperSC,
+});
