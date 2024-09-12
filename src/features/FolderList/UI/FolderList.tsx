af674d7592f781eb7a604a5157c9c605a79dda99
Gribbariga <mormir09@gmail.com>
2024-09-13 05:32:01 +0900

    test

--- src/features/FolderList/UI/FolderList.tsx
+++ src/features/FolderList/UI/FolderList.tsx
@@ -1,21 +1,28 @@
 import { ScrollArea, Text } from "@radix-ui/themes";
 import { FolderListStyle } from "./FolderListStyle.ts";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAllFolder } from "src/shared/API/storage/folder/api.ts";
 import { storageSlice } from "src/entities/storage/model/storageSlice.ts";
 import { FolderItem } from "./FolderItem/FolderItem.tsx";
 
 export const FolderList = () => {
   const { allFolder, setAllFolder } = storageSlice((state) => state);
+  const [isLoading, setIsLoading] = useState(false);
   const headerText = ["Имя", "Размер", "Создано", "Срок хранения"];
 
   useEffect(() => {
     if (!allFolder.length) {
+      setIsLoading(true);
       getAllFolder().then(({ data }) => {
+        setIsLoading(false);
         setAllFolder(data.folders);
       });
     }
   }, []);
+
+  const skeletonNumber = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
 
   return (
     <>
@@ -38,21 +45,36 @@ export const FolderList = () => {
         })}
       </ListHeaderSC>
       <ScrollArea>
-        {allFolder.map((item) => {
-          return (
-            <FolderItem
-              folderId={item.folder_id}
-              key={item.folder_id}
-              name={item.name}
-              size={item.size}
-              createAt={item.created_at}
-              timeStorage={item.lifetime}
-            />
-          );
-        })}
+        {true &&
+          skeletonNumber.map((item) => {
+            return (
+              <SkeletonWrapperSC key={item}>
+                <SkeletonSC />
+              </SkeletonWrapperSC>
+            );
+          })}
+
+        {false &&
+          allFolder.map((item) => {
+            return (
+              <FolderItem
+                folderId={item.folder_id}
+                key={item.folder_id}
+                name={item.name}
+                size={item.size}
+                createAt={item.created_at}
+                timeStorage={item.lifetime}
+              />
+            );
+          })}
       </ScrollArea>
     </>
   );
 };
 
-const { ListHeaderSC, ListHeaderSegmentWrapperSC } = FolderListStyle();
+const {
+  SkeletonSC,
+  ListHeaderSC,
+  SkeletonWrapperSC,
+  ListHeaderSegmentWrapperSC,
+} = FolderListStyle();
--- src/features/FolderList/UI/FolderListStyle.ts
+++ src/features/FolderList/UI/FolderListStyle.ts
@@ -1,3 +1,4 @@
+import { Skeleton } from "@radix-ui/themes";
 import styled from "styled-components";
 
 const ListHeaderSC = styled("div")`
@@ -14,7 +15,25 @@ const ListHeaderSegmentWrapperSC = styled("div")`
   width: 25%;
 `;
 
+const SkeletonWrapperSC = styled("div")`
+  height: 44px;
+  margin-bottom: var(--space-3);
+  display: flex;
+  padding: var(--space-3);
+  align-items: center;
+  align-self: stretch;
+  border-bottom: 1px solid var(--gray-a5, rgba(0, 0, 0, 0.15));
+  background: #fff;
+`;
+
+const SkeletonSC = styled(Skeleton)`
+  width: 100%;
+  height: 20px;
+`;
+
 export const FolderListStyle = () => ({
+  SkeletonSC,
   ListHeaderSC,
+  SkeletonWrapperSC,
   ListHeaderSegmentWrapperSC,
 });
