3c6e2e0c7b9f2d05c9bfc263f606e0191c761d63
Gribbariga <mormir09@gmail.com>
2024-09-22 18:14:11 +0900

    subscription fix after change api

--- src/shared/API/subscription/model.ts
+++ src/shared/API/subscription/model.ts
@@ -1,32 +1,20 @@
 export interface ITariffs {
   name: string;
-
   price: number;
-
   max_storage_size: string;
-
   max_folder_size: string;
-
   max_file_size: string;
-
   max_folder_count: number;
-
   max_file_in_folder: number;
-
   max_file_at_time: number;
-
   max_lifetime: number;
-
   max_sub_account_count: number;
+  tariff_id: number;
 }
 
 export type TGetTarrifs = {
   [key: number]: ITariffs;
 };
-
-export interface ISubscriptionStatusArg {
-  user_id: number;
-}
 
 export interface ISubscriptionStatusResponse {
   name: string;
@@ -39,8 +27,8 @@ export interface ISubscriptionStatusResponse {
   max_file_at_time: number;
   max_lifetime: number;
   max_sub_account_count: number;
-  subscription_id: number;
-  subscription_duration: number;
+  tariff_id: number;
+  duration: number;
   created_at: string;
   updated_at: string;
 }
--- src/shared/API/subscription/subscription.ts
+++ src/shared/API/subscription/subscription.ts
@@ -1,16 +1,10 @@
 import { axiosBase } from "shared/config/axiosConfig/axiosConfig";
-import {
-  ISubscriptionStatusArg,
-  ISubscriptionStatusResponse,
-  TGetTarrifs,
-} from "./model";
+import { ISubscriptionStatusResponse, TGetTarrifs } from "./model";
 
 export const getTarrifs = async () => {
   return await axiosBase.get<TGetTarrifs>("/api/subscription/tariffs");
 };
 
-export const subscriptionStatus = async (arg: ISubscriptionStatusArg) => {
-  return await axiosBase.get<ISubscriptionStatusResponse>(
-    `/api/subscription/status/${arg.user_id}`
-  );
+export const subscriptionStatus = async () => {
+  return await axiosBase.get<ISubscriptionStatusResponse>(`/status`);
 };
