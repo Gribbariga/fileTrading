7b743169d7c3ce10b36194f0ab82ebab0c478e59
Gribbariga <mormir09@gmail.com>
2024-09-27 00:33:02 +0900

    f

--- src/shared/API/account/setting/setting.ts
+++ src/shared/API/account/setting/setting.ts
@@ -2,7 +2,5 @@ import { axiosBaseAccount } from "src/shared/config/axiosConfig/axiosConfig";
 import { IChangePasswordArg } from "./model";
 
 export const changePassword = async (arg: IChangePasswordArg) => {
-  return axiosBaseAccount.patch<null>("/password/change", {
-    data: arg,
-  });
+  return axiosBaseAccount.patch<null>("/password/change", arg);
 };
