07ab6799adf9cf233eed833ba0f53909c838e045
Gribbariga <mormir09@gmail.com>
2024-11-01 11:45:37 +0900

    add subscribe info in header

--- src/widgets/layout/UI/Header/ProfileConfig/Subscrib/TabsSubscribeContent.tsx
+++ src/widgets/layout/UI/Header/ProfileConfig/Subscrib/TabsSubscribeContent.tsx
@@ -2,15 +2,55 @@ import { Progress, Text } from "@radix-ui/themes";
 import { TabsSubscribeContentStyle } from "./TabsSubscribeContentStyle.ts";
 import { ButtonUI } from "src/shared/ButtonUI/ButtonUI.tsx";
 import { useNavigate } from "react-router-dom";
+import { subscriptionSlice } from "src/entities/subscription/model/subcriptionSlice.ts";
+import { useEffect, useState } from "react";
+import { getAllFolder } from "src/shared/API/storage/folder/api.ts";
+import { storageSlice } from "src/entities/storage/model/storageSlice.ts";
+
+type ITariffName = "Free" | "Premium" | "Business" | "Corporate";
 
 export const TabsSubscribeContent = () => {
   const navigation = useNavigate();
 
+  const { tariffs, subscribeStatus } = subscriptionSlice((state) => state);
+  const { allFolder } = storageSlice((state) => state);
+
+  const [percent, setPercent] = useState(0);
+
+  const TariffName: Record<ITariffName, string> = {
+    Free: "Бесплатный",
+    Premium: "Премиум",
+    Business: "Бизнес",
+    Corporate: "Корпоративный",
+  };
+
   const infoItemSet = [
-    { name: "Тариф:", value: "Бизнес" },
-    { name: "Дней осталось:", value: "11" },
-    { name: "Заполнено:", value: "75%" },
+    {
+      name: "Тариф:",
+      value: TariffName[(subscribeStatus?.name as ITariffName) || "free"],
+    },
+    {
+      name: "Месяцев осталось:",
+      value: subscribeStatus?.duration || 0 < 0 ? 0 : subscribeStatus?.duration,
+    },
+    { name: "Заполнено:", value: `${percent}%` },
   ];
+
+  useEffect(() => {
+    if (allFolder.length <= 0 && tariffs && subscribeStatus) {
+      const currentTariff =
+        tariffs !== null ? tariffs[subscribeStatus?.tariff_id || 0] : null;
+      getAllFolder()
+        .then(({ data }) => {
+          let res = 0;
+          data.folders.map((item) => {
+            res += +item.size;
+          });
+          setPercent((res / +(currentTariff?.max_storage_size || 1)) * 100);
+        })
+        .catch(() => {});
+    }
+  }, [tariffs, subscribeStatus]);
 
   return (
     <>
